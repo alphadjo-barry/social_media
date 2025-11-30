707ae3fc0bbff81920c284675eaee241a12dc4f9
Alphadjo Barry <alphadiobiya@gmail.com>
2025-11-30 12:48:33 +0100

    use environnement variables for connect to postgres

---
+++ src/main/java/com/alphadjo/social_media/configuration/AppConfig.java
@@ -0,0 +1,17 @@
+package com.alphadjo.social_media.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource(".env"));
+        return configurer;
+    }
+}
